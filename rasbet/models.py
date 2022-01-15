61eb2a7407c325413e3811b551a727ff6d84e25a
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2022-01-15 01:40:19 +0000

    Models e db

--- rasbet/models.py
+++ rasbet/models.py
@@ -19,6 +19,82 @@ class User(db.Model, UserMixin):
     address = db.Column(db.String(100), nullable = False)
     phone = db.Column(db.String(15), unique=True, nullable = False)
     role = db.Column(db.String(20), unique = False)
+    wallet = db.relationship("Wallet")
+    movements = db.relationship("Movement")
+    bets = db.relationship("Bet")
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+
+class Wallet(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    balance = db.Column(db.Float, unique = False, nullable = False)
+    currency = db.Column(db.String(20), unique = False, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    wm = db.relationship("WalletMovement")
+
+class Movement(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    value = db.Column(db.Float, unique = False, nullable = False)
+    type = db.Column(db.String(20), unique = False, nullable = False)
+    date = db.Column(db.DateTime, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    wm = db.relationship("WalletMovement")
+
+class WalletMovememnt(db.Model):
+    movement_id = db.Column(db.Integer, db.ForeignKey('movement.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    wallet_id = db.Column(db.Integer, db.ForeignKey('wallet.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+
+class Bet(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    value = db.Column(db.Float, unique = False, nullable = False)
+    state = db.Column(db.String(20), unique = False, nullable = False)
+    date = db.Column(db.DateTime, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    bo = db.relationship("BetOpp")
+
+class BetOdd(db.Model):
+    bet_id = db.Column(db.Integer, db.ForeignKey('bet.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    odd_id = db.Column(db.Integer, db.ForeignKey('odd.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+
+class Odd(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    value = db.Column(db.Float, unique = False, nullable = False)
+    description = db.Column(db.String(20), unique = False, nullable = False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id', ondelete='CASCADE'), nullable=False)
+    bo = db.relationship("BetOpp")
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    start_date = db.Column(db.DateTime, nullable = False)
+    end_date = db.Column(db.DateTime, nullable = False)
+    state = db.Column(db.String(20), unique = False, nullable = False)
+    competition_id = db.Column(db.Integer, db.ForeignKey('competition.id', ondelete='CASCADE'), nullable=False)
+    odds = db.relationship("Odd")
+    ep = db.relationship("ParticipantEvent")
+
+class Competition(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120), unique = False, nullable = False)
+    sport_id = db.Column(db.Integer, db.ForeignKey('sport.id', ondelete='CASCADE'), nullable=False)
+    events = db.relationship("Event")
+    pc = db.relationship("ParticipantCompetition")
+
+class Sport(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120), unique = True, nullable = False)
+    competitions = db.relationship("Competition")
+
+class Participant(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120), unique = True, nullable = False)
+    pc = db.relationship("ParticipantCompetition")
+    pe = db.relationship("ParticipantEvent")
+
+class ParticipantCompetition(db.Model):
+    competition_id = db.Column(db.Integer, db.ForeignKey('competition.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    participant_id = db.Column(db.Integer, db.ForeignKey('participant.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+
+class ParticipantEvent(db.Model):
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    participant_id = db.Column(db.Integer, db.ForeignKey('participant.id', ondelete='CASCADE'), primary_key=True, nullable=False)
