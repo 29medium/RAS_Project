2f2309bb152721e61eba26b3e43cd21f53d95844
56Aang <a89588@alunos.uminho.pt>
2022-01-15 01:19:31 +0000

    api graber + api json

--- rasbet/__init__.py
+++ rasbet/__init__.py
@@ -1,3 +1,5 @@
+import threading
+from apiGraber import loadApi
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -11,5 +13,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+apiThread = threading.Thread(target=loadApi,args=db)
 
 from rasbet import routes
